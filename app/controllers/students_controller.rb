c1199b94c099abac6f6eaabe987e30fba4cbf8e7
github username <github email address>
2018-06-10 20:52:02 -0400

    implemented student form acceptor with form tag helpers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
+require 'pry'
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,4 +9,14 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def create
+    @student = Student.create(student_params)
+
+    redirect_to student_path(@student)
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
 end
